16d3c0ef93fef093b4b50623fceb453b90fd290e
Martins <thony.martins.67@gmail.com>
2024-02-28 13:54:31 +0100

    adding component header and scss file colors, general, variables

--- front-end/src/App/AppRouter/index.jsx
+++ front-end/src/App/AppRouter/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from '../../pages/Home'
+import Header from '../../containers/layouts/Header'
 
 function AppRouter() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
---
+++ front-end/src/containers/layouts/Header/index.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="header-container">
+      <div className="header-style">webcraft-anthony.fr</div>
+      <i class="fa-solid fa-bars header-icon"></i>
+    </header>
+  )
+}
+
+export default Header
