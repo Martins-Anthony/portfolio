b50eb87bbfa33ef0cac91b459b45d0e1e0dfdffb
Martins <thony.martins.67@gmail.com>
2024-03-05 18:49:27 +0100

    add media query desktop for header and about section

--- front-end/src/containers/About/index.jsx
+++ front-end/src/containers/About/index.jsx
@@ -1,8 +1,8 @@
 function About() {
   return (
     <section className="about-container">
-      <h1 className="about">Hello, my name is Anthony Martins,</h1>
-      <p className="about">
+      <h1>Hello, my name is Anthony Martins,</h1>
+      <p>
         a passionate front-end developer specializing in React. I am currently looking for a
         work-study opportunity to obtain the Sofware Designer Developer certificate.
       </p>
