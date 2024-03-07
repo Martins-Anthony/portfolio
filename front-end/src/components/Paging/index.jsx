32a59ba14a39c2ddb3af99a1d1b5091f6519ed2c
Martins <thony.martins.67@gmail.com>
2024-03-07 11:18:47 +0100

    modification component maps for paging

---
+++ front-end/src/components/Paging/index.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import Cards from '../Cards'
+
+function Paging({ data }) {
+  const itemsPerPage = 4
+  const totalPages = Math.ceil(data.length / itemsPerPage)
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1)
+  }
+
+  const handlePrevPage = () => {
+    setCurrentPage(currentPage - 1)
+  }
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber)
+  }
+
+  const startIndex = (currentPage - 1) * itemsPerPage
+  const endIndex = startIndex + itemsPerPage
+
+  const currentPageData = data.slice(startIndex, endIndex)
+
+  const columnData = []
+  for (let i = 0; i < currentPageData.length; i += 2) {
+    columnData.push(currentPageData.slice(i, i + 2))
+  }
+
+  return (
+    <>
+      <div className="paging-container">
+        {columnData.map((column, index) => (
+          <div key={index} className="paging-column">
+            <Cards projects={column} />
+          </div>
+        ))}
+      </div>
+      {totalPages > 1 ? (
+        <div>
+          <button onClick={handlePrevPage} disabled={currentPage === 1}>
+            Previous
+          </button>
+          {Array.from({ length: totalPages }, (_, index) => (
+            <button key={index + 1} onClick={() => handlePageChange(index + 1)}>
+              {index + 1}
+            </button>
+          ))}
+          <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+            Next
+          </button>
+        </div>
+      ) : null}
+    </>
+  )
+}
+
+export default Paging
--- front-end/src/containers/Projects/index.jsx
+++ front-end/src/containers/Projects/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Cards from '../../components/Cards'
 import data from '../../data/projects'
+import Paging from '../../components/Paging'
 
 function Projects({ title }) {
   return (
     <section className="projects-container">
       <div className="projects-container-style">
         <h2>{title}</h2>
-        <Cards projects={data} />
+        <Paging data={data} />
       </div>
     </section>
   )
