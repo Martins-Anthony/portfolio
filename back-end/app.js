3d2a63c7247c8d2b318da4431549eaa116a007e6
Martins <thony.martins.67@gmail.com>
2024-03-10 19:17:16 +0100

    improvement server.js

--- back-end/app.js
+++ back-end/app.js
@@ -1,8 +1,7 @@
 const express = require('express')
 
 const app = express()
-
-app.use((req, res) => {
+app.use((req, res, next) => {
   res.json({ message: 'Votre requête a bien été reçue !'})
 })
 
